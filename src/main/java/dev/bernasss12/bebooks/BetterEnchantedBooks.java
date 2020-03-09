0d74348855df05b7d8985475a868be933d552442
Bernardo Antunes <bernasss12@gmail.com>
2020-03-09 15:50:44 +0000

    Made mod purely client sided.

--- src/main/java/dev/bernasss12/bebooks/BetterEnchantedBooks.java
+++ src/main/java/dev/bernasss12/bebooks/BetterEnchantedBooks.java
@@ -1,6 +1,6 @@
 package dev.bernasss12.bebooks;
 
-import net.fabricmc.api.ModInitializer;
+import net.fabricmc.api.ClientModInitializer;
 import net.fabricmc.fabric.api.client.rendering.v1.ColorProviderRegistry;
 import net.minecraft.item.EnchantedBookItem;
 import net.minecraft.item.ItemStack;
@@ -9,23 +9,28 @@ import net.minecraft.nbt.CompoundTag;
 import net.minecraft.nbt.ListTag;
 import net.minecraft.util.registry.Registry;
 
-import java.util.Collections;
-import java.util.List;
-
-public class BetterEnchantedBooks implements ModInitializer {
+public class BetterEnchantedBooks implements ClientModInitializer {
 	@Override
-	public void onInitialize() {
+	public void onInitializeClient() {
 		ColorProviderRegistry.ITEM.register((stack, tintIndex) -> tintIndex > 0 ? getColorFromEnchantmentList(stack) : -1, Items.ENCHANTED_BOOK);
+		Registry.ENCHANTMENT.iterator();
 	}
-
+	
 	/* Get color associated with most valuable enchantment in the list enchantment. */
-	public int getColorFromEnchantmentList(ItemStack stack){
-		if(stack.isItemEqual(new ItemStack(Items.ENCHANTED_BOOK))){
-			CompoundTag tag = stack.getTag();
-			if(tag != null && tag.contains("EnchantmentColor")) {
-						CompoundTag color = tag.getCompound("EnchantmentColor");
-						return color.contains("color") ? color.getInt("color") : -1;
-					}else return -1;
-		}else return -1;
+	public int getColorFromEnchantmentList(ItemStack stack) {
+		if (stack.isItemEqual(new ItemStack(Items.ENCHANTED_BOOK))) {
+			ListTag enchantments = EnchantedBookItem.getEnchantmentTag(stack);
+			CompoundTag firstEnchantment = enchantments.getCompound(0);
+			switch (firstEnchantment.getString("id")) {
+				case "minecraft:mending":
+					return 0x0000fe;
+				case "minecraft:fortune":
+					return 0xfe2020;
+				case "minecraft:silk_touch":
+					return 0x15fe15;
+				default:
+					return 0x5e0000;
+			}
+		} else return -1;
 	}
 }
