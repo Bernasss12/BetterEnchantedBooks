1c8cabb98dce6d60fe9543a270f545c86538ef83
Fourmisain <8464472+Fourmisain@users.noreply.github.com>
2023-06-09 13:47:35 +0200

    fix rendering of tooltip icons

--- src/main/java/dev/bernasss12/bebooks/client/gui/tooltip/IconTooltipComponent.java
+++ src/main/java/dev/bernasss12/bebooks/client/gui/tooltip/IconTooltipComponent.java
@@ -1,6 +1,5 @@
 package dev.bernasss12.bebooks.client.gui.tooltip;
 
-import com.mojang.blaze3d.systems.RenderSystem;
 import net.minecraft.client.font.TextRenderer;
 import net.minecraft.client.gui.tooltip.TooltipComponent;
 import net.minecraft.client.render.item.ItemRenderer;
@@ -22,18 +21,16 @@ public record IconTooltipComponent(List<ItemStack> icons) implements TooltipComp
     }
 
     @Override
-    public void drawItems(TextRenderer textRenderer, int x, int y, MatrixStack _matrices, ItemRenderer itemRenderer) {
+    public void drawItems(TextRenderer textRenderer, int x, int y, MatrixStack matrices, ItemRenderer itemRenderer) {
         float scale = 0.5f;
         int scaledX = (int) (x / scale);
         int scaledY = (int) (y / scale);
         int scaledOffset =  (int) (8 / scale);
-        MatrixStack matrices = RenderSystem.getModelViewStack();
         matrices.push();
         matrices.scale(0.5f, 0.5f, 1.0f);
         for(int i = 0; i < icons.size(); i++){
             itemRenderer.renderInGuiWithOverrides(matrices, icons.get(i), scaledX + scaledOffset * i, scaledY, -1);
         }
         matrices.pop();
-        RenderSystem.applyModelViewMatrix();
     }
 }
