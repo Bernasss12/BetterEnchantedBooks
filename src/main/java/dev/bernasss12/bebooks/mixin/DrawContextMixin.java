1e22df59ab8f08794f1a2236cb46cc3c81ee00dc
Fourmisain <8464472+Fourmisain@users.noreply.github.com>
2023-06-09 14:47:36 +0200

    handle tooltips for merchants as well

--- src/main/java/dev/bernasss12/bebooks/mixin/DrawContextMixin.java
+++ src/main/java/dev/bernasss12/bebooks/mixin/DrawContextMixin.java
@@ -6,15 +6,19 @@ import dev.bernasss12.bebooks.client.gui.tooltip.IconTooltipComponent;
 import dev.bernasss12.bebooks.util.text.IconTooltipDataText;
 import net.fabricmc.api.EnvType;
 import net.fabricmc.api.Environment;
+import net.minecraft.client.font.TextRenderer;
 import net.minecraft.client.gui.DrawContext;
 import net.minecraft.client.gui.screen.Screen;
 import net.minecraft.client.gui.tooltip.OrderedTextTooltipComponent;
 import net.minecraft.client.gui.tooltip.TooltipComponent;
+import net.minecraft.item.ItemStack;
 import net.minecraft.item.Items;
 import net.minecraft.text.OrderedText;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.ModifyVariable;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
 import java.util.List;
 import java.util.stream.Collectors;
@@ -22,6 +26,20 @@ import java.util.stream.Collectors;
 @Mixin(DrawContext.class)
 @Environment(EnvType.CLIENT)
 public abstract class DrawContextMixin {
+    // called for Merchant items, but not for inventory items
+    @Inject(
+        method = "drawItemTooltip(Lnet/minecraft/client/font/TextRenderer;Lnet/minecraft/item/ItemStack;II)V",
+        at = @At("HEAD")
+    )
+    private void setEnchantedItemStack(TextRenderer textRenderer, ItemStack stack, int x, int y, CallbackInfo ci) {
+        BetterEnchantedBooks.enchantedItemStack.set(stack);
+    }
+
+    @Inject(method = "drawItemTooltip(Lnet/minecraft/client/font/TextRenderer;Lnet/minecraft/item/ItemStack;II)V", at = @At(value = "TAIL"))
+    private void forgetEnchantedItemStack(TextRenderer textRenderer, ItemStack stack, int x, int y, CallbackInfo ci) {
+        BetterEnchantedBooks.enchantedItemStack.set(ItemStack.EMPTY);
+    }
+
     @ModifyVariable(
         at = @At("HEAD"),
         method = "drawTooltip(Lnet/minecraft/client/font/TextRenderer;Ljava/util/List;IILnet/minecraft/client/gui/tooltip/TooltipPositioner;)V",
